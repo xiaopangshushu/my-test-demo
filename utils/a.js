d60442072812d955285ea7dce1ca03e3f6ca1844
xiaoyongjin <xyjcat2017@163.com>
2024-04-03 09:38:04 +0800

    fix:又写了点什么

--- utils/a.js
+++ utils/a.js
@@ -1,3 +1,24 @@
 /**
  * 新增a.js 提交记录b
  */
+function deepClone(obj={},map=new Map()){
+    if(typeof obj !='object'){
+        return obj;
+    }
+
+    if(map.get(obj)){
+        return map.get(obj)
+    }
+    let result = {};
+    if(Array.isArray(obj)||obj instanceof Array||Object.prototype.toString(obj)==='[object Array]'){
+        result=[]
+    }
+
+    map.set(obj,result);//防止重复
+    for(const key in obj){
+        if(obj.hasOwnProperty(key)){
+            result[key] = deepClone(obj[key],map)
+        }
+    }
+    return result;
+}
